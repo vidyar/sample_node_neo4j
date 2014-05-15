fa6e6a11af7acb06eaba90ebe47d3e5fed025765
tmlbl <tim.lebel@gmail.com>
2014-05-15 11:41:04 -0700

    initial commit

---
+++ test.js
@@ -0,0 +1,18 @@
+var db = require('./index.js').db,
+    expect = require('chai').expect;
+
+describe('Neo4j database', function () {
+  it('should save a node', function () {
+    var node = db.createNode({ name: 'test' });
+    node.save(function (err, node) {
+      expect(err).to.equal(null);
+      expect(node).to.have.property('id');
+    });
+  });
+  it('should retrieve a node', function () {
+    db.getIndexedNodes('node_auto_index', 'name', 'test', function (nodes) {
+      expect(nodes).to.be.an('array');
+      expect(nodes.length).to.equal(1);
+    });
+  });
+});
